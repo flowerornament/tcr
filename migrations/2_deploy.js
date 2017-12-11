65bf85c327963a1d926607b593d0a489f005ae6f
Morgan Sutherland <morgan@morgansutherland.com>
2017-12-11 01:27:56 +0100

    add secrets, install truffle-hdwallet-provider

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,7 +2,9 @@ var TCR = artifacts.require("./TCR.sol");
 var TestToken = artifacts.require("./TestToken.sol");
 
 module.exports = function(deployer) {
-  return deployer.deploy(TestToken).then(function () {
-    return deployer.deploy(TCR, TestToken.address);
-  });
+  if (false) {
+    return deployer.deploy(TestToken).then(function () {
+      return deployer.deploy(TCR, TestToken.address);
+    });
+  }
 };
--- truffle.js
+++ truffle.js
@@ -1,3 +1,18 @@
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+//   networks: {
+//     development: {
+//       host: 'localhost',
+//       port: '7545',
+//       network_id: '*'
+//     }
+//   }
+// };
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var secrets = require('./secrets.js')
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +21,20 @@ module.exports = {
       host: 'localhost',
       port: '7545',
       network_id: '*'
+    },
+    kovan: {
+      gas: 470000,
+      provider: function() {
+        return new HDWalletProvider(secrets.mnemonic, "https://kovan.infura.io/" + secrets.infura)
+      },
+      network_id: 42
+    },
+    rinkeby: {
+      // gas: 470000,
+      provider: function() {
+        return new HDWalletProvider(secrets.mnemonic, "https://rinkeby.infura.io/" + secrets.infura)
+      },
+      network_id: 4
     }
   }
 };
